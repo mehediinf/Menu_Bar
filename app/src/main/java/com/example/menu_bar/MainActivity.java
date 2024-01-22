65fbb8a4fe3db15e997d2a01cad782467fa76cb0
mehedi-hasan <mehediinform@gmail.com>
2024-01-22 21:00:47 +0600

    First Commit Menu Option Bar

---
+++ app/src/main/java/com/example/menu_bar/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.menu_bar;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_layout,menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+}
